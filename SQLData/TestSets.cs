de886f01ce92784c57b5a5041fc0dbb03d74f549
Balyi Mark <balyi.mark@hotmail.com>
2017-02-26 08:48:33 +0100

    Nodes

---
+++ SQLData/TestSets.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestManagementStudio.SQLData
+{
+    public class TestSets
+    {
+        [Key]
+        public int TestSetId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Comment { get; set; }
+        [Required]
+        public DateTime CreatedDate { get; set; }
+        [Required]
+        public DateTime ModifiedDate { get; set; }
+        [Required]
+        public Boolean IsDeleted { get; set; }
+        [Required]
+        public Boolean IsArchived { get; set; }
+        [Required]
+        public Boolean IsUpdated { get; set; }
+        [Required]
+        public int UpdatedTo { get; set; }
+        [Required]
+        public int Interval { get; set; }
+        [Required]
+        public int CreatedBy { get; set; }
+        [Required]
+        public int SubjectId { get; set; }
+    }
+}
