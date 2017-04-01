d01c664d267ac7b4136ea2f77f5f1733b69d6d10
Balyi Mark <balyi.mark@hotmail.com>
2017-04-01 17:33:10 +0200

    Questions wired

--- angular2-webpack-starter/src/app/components/manager/questions.component.ts
+++ angular2-webpack-starter/src/app/components/manager/questions.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DropdownModule, SelectItem } from 'primeng/primeng';
 
+import { NotificationActions } from './../../actions/notification.actions';
 import { QuestionService } from './../../services/question.service';
 import { CategoryService } from './../../services/category.service';
 
@@ -27,9 +28,16 @@ export class QuestionsComponent {
 
     private fakeBackend: FakeAdminServer = new FakeAdminServer();
 
-    constructor(private questionService: QuestionService, private categoryService: CategoryService) {
+    constructor(private questionService: QuestionService, private categoryService: CategoryService,
+        private notificationAction: NotificationActions) {}
+    
+    ngOnInit() {
         //TO DO: get categories from server
-        //categoryService.getAll().subscribe((data) => this.categories = data);
+        // this.categoryService.getAll().subscribe(
+        //     (data) => this.categories = data,
+        //     err => this.notificationAction.setNotification(false, 'Request failed.', err.toString())
+        // );
+
         this.categories = this.fakeBackend.getCategories();
         this.categoryItems.push({label: 'All Categories', value: { id: 0, name: 'All Categories' } });
         for(let i = 0; i < this.categories.length; i++) {
@@ -46,7 +54,7 @@ export class QuestionsComponent {
                 (data) => this.questions = data
             );
         } else {
-            this.questionService.getByCategoryId(this.selectedCategoryItem.id).subscribe(
+            this.categoryService.getQuestionsByCategory(this.selectedCategoryItem.id).subscribe(
                 (data) => this.questions = data
             );
         }*/
@@ -85,10 +93,43 @@ export class QuestionsComponent {
     editQuestion(question: Question) {
         this.displayDialog = true;
         this.isNew = false;
-        this.question = question;
+        this.question = Object.assign({}, question);
+    }
+
+    getIndexOfQuestion(question: Question) {
+        for (let i = 0; i < this.questions.length; i++) {
+            if (this.questions[i].id == question.id) {
+                return i;
+            }
+        }
+        return null;
     }
 
     addNewQuestion(question: Question) {
+        // //TO DO: save/update questions
+        // if (this.isNew) {
+        //     this.questionService.update(question).subscribe(
+        //         q => {
+        //             let id = this.getIndexOfQuestion(q);
+        //             if (id) {
+        //                 this.notificationAction.setNotification(true, 'Question updated.', 'Question successfully updated.');
+        //                 this.displayDialog = false;
+        //             } else {
+        //                 this.notificationAction.setNotification(false, 'Request failed.', 'Can not update the Question.')
+        //             }
+        //         },
+        //         err => this.notificationAction.setNotification(false, 'Request failed.', err.toString())
+        //     );
+        // } else {
+        //     this.questionService.save(question).subscribe(
+        //         q => {
+        //             this.questions.push(q);
+        //             this.notificationAction.setNotification(true, 'Question stored.', 'Question successfully saved.');
+        //             this.displayDialog = false;
+        //         },
+        //         err => this.notificationAction.setNotification(false, 'Request failed.', err.toString())
+        //     )
+        // }
         if (this.isNew)
             this.questions.push(question);
         else
@@ -101,6 +142,15 @@ export class QuestionsComponent {
     }
 
     deleteQuestion(question: Question) {
+        // // TO DO: delete question
+        // this.questionService.delete(question).subscribe(
+        //     (data) => {
+        //         this.questions.splice(this.questions.indexOf(question),1);
+        //         this.notificationAction.setNotification(true, 'Question deleted.', 'Question successfully deleted.');
+        //         this.displayDialog = false;
+        //     },
+        //     err => this.notificationAction.setNotification(false, 'Request failed.', err.toString())
+        // );
         this.questions.splice(this.questions.indexOf(question),1);
     }
 }
